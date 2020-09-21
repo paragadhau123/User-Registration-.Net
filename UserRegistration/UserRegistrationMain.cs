2a609a24fd83827eb4b1730d6b1ead89f8f13527
Parag Adhau <adhauparag64@gmail.com>
2020-09-21 22:47:11 +0530

    Added[UC4] :Code for phone number validation

--- UserRegistration/UserRegistrationMain.cs
+++ UserRegistration/UserRegistrationMain.cs
@@ -7,7 +7,7 @@
     {
         public readonly string NAMEPATTERN = "^[A-Z]{1}[a-z]{2,}$";
         public readonly string EMAILPATTERN = "^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$";
-
+        public readonly string PHONE_PATTERN = "^[1-9][0-9]+[ ]{1}+[1-9][0-9]{9}$";
         public bool FirstNameValidator(string firstName)
         {
             return Regex.Match(firstName, this.NAMEPATTERN).Success;
@@ -18,9 +18,14 @@
             return Regex.Match(lastName, this.NAMEPATTERN).Success;
         }
 
-        public Boolean EmailValidator(string email)
+        public bool EmailValidator(string email)
         {
             return Regex.Match(email, this.EMAILPATTERN).Success;
+        }
+
+        public bool Mobile(string mobile)
+        {
+            return Regex.Match(mobile, PHONE_PATTERN).Success;
         }
     }
 }
