1b0290a66110fd13477e2b647500a721c7a11643
Parag Adhau <adhauparag64@gmail.com>
2020-09-21 21:33:41 +0530

    Added[UC3] :Code to validate user email

--- UserRegistration/UserRegistrationMain.cs
+++ UserRegistration/UserRegistrationMain.cs
@@ -6,14 +6,21 @@
     public class UserRegistrationMain
     {
         public readonly string NAMEPATTERN = "^[A-Z]{1}[a-z]{2,}$";
+        public readonly string EMAILPATTERN = "^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$";
 
         public bool FirstNameValidator(string firstName)
         {
             return Regex.Match(firstName, this.NAMEPATTERN).Success;
         }
+
         public bool LastNameValidator(string lastName)
         {
             return Regex.Match(lastName, this.NAMEPATTERN).Success;
+        }
+
+        public Boolean EmailValidator(string email)
+        {
+            return Regex.Match(email, this.EMAILPATTERN).Success;
         }
     }
 }
