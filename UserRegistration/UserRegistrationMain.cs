37209268b89bf9d77f1f45d021f539eb42951d8c
Parag Adhau <adhauparag64@gmail.com>
2020-09-21 23:32:52 +0530

    Added[UC5] :Code to validate password

--- UserRegistration/UserRegistrationMain.cs
+++ UserRegistration/UserRegistrationMain.cs
@@ -7,8 +7,8 @@
     {
         public readonly string NAMEPATTERN = "^[A-Z]{1}[a-z]{2,}$";
         public readonly string EMAILPATTERN = "^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$";
-        public readonly string PHONE_PATTERN = "^([0-9]{2}[ ]+)?[0-9]{10}$";
-
+        public readonly string PHONEPATTERN = "^([0-9]{2}[ ]+)?[0-9]{10}$";
+        public readonly string PASSWORDPATTERN = "^([A-Z]{1,})*([(@#$*)-])*[a-zA-Z0-9]{6,}$";
         public bool FirstNameValidator(string firstName)
         {
             return Regex.Match(firstName, this.NAMEPATTERN).Success;
@@ -26,7 +26,12 @@
 
         public bool MobileValidator(string mobile)
         {
-            return Regex.Match(mobile, PHONE_PATTERN).Success;
+            return Regex.Match(mobile, PHONEPATTERN).Success;
+        }
+
+        public bool PasswordValidator(string password)
+        {
+            return Regex.Match(password, PASSWORDPATTERN).Success;
         }
     }
 }
